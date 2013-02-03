74e942c9dad492040e7bdc0952e2eeaed2dc228a
Andres Riofrio <riofrios@gmail.com>
2013-02-03 15:06:46 -0800

    Implement lexer, add --only-scanner option

--- main.cpp
+++ main.cpp
@@ -1,16 +1,46 @@
 #include "parser.hpp"
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 
 extern int yydebug; // set this to 1 if you want yyparse to dump a trace
 extern int yyparse(); // this actually the parser which then calls the scanner
+extern int yylex();
+extern void print_token(int);
 
-int main(void) {
+void printUsage(char* command) {
+  printf("Usage: %s [--debug] [--only-scanner]\n", command);
+}
 
-  // set this to 1 if you would like to print a trace 
-  // of the entire parsing process (it prints to stdout)
-  yydebug = 0; 
+int main(int argc, char** argv) {
+  bool onlyScanner = false;
 
-  yyparse();  
+  static struct option long_options[] = {
+    {"debug", no_argument, 0, 'd'},
+    {"only-scanner", no_argument, 0, 's'},
+    {0, 0, 0, 0}
+  };
+
+  int long_index = 0, opt = 0;
+  while((opt = getopt_long(
+          argc, argv, "ds", long_options, &long_index)) != -1) {
+    switch(opt) {
+      case 'd': yydebug = 1; break;
+      case 's': onlyScanner = true; break;
+      default: printUsage(argv[0]); exit(EXIT_FAILURE); break;
+    }
+  }
+
+  if(onlyScanner) {
+    unsigned int token;
+    while((token = yylex()) != 0) {
+      print_token(token);
+    }
+    printf("\n");
+  } else {
+    yyparse();
+  }
 
   return 0;
 
