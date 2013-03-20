e6bc7c4ace15c32a6a52a054656cca617cbcd370
Andres Riofrio <riofrios@gmail.com>
2013-03-19 23:14:55 -0700

    Check that all programs can be compiled

--- tests/run.sh
+++ tests/run.sh
@@ -75,18 +75,35 @@ function simplec() {
   bash -c "$runfile"
 }
 
-section "Valid programs compile and run correctly"
+section "Valid programs compile"
 for test in $(find tests/good -name '*.simple' | sort); do
   runfile=${test%.simple}.run
   ofile=${test%.simple}.o
   sfile=${test%.simple}.s
   outfile=${test%.simple}.out
+
+  error=$(simplec 2>&1 > /dev/null)
+  test="$SIMPLE < $test > $sfile"
+  if [ "$error" ]; then
+    report_incorrect
+  else
+    report_correct
+  fi
+done > >(indent)
+
+section "Valid programs run correctly"
+for test in $(find tests/good -name '*.simple' | sort); do
+  runfile=${test%.simple}.run
+  ofile=${test%.simple}.o
+  sfile=${test%.simple}.s
+  outfile=${test%.simple}.out
+
   if [ -f $outfile ]; then
     tempfile=$(mktemp)
     error=$(simplec 2>&1 > $tempfile)
     test="$SIMPLE < $test > $sfile"
     if [ "$error" ]; then
-      report_incorrect
+      report_skipped
     else
       diff $tempfile $outfile &> /dev/null && report_correct || report_incorrect
       colordiff $tempfile $outfile | indent
